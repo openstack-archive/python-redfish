3915eaad8edbab0c2afb94914851f7bf2b2627ff
Yufei <zhang.yufei@99cloud.net>
2017-01-13 00:36:50 +0800

    Fix wrong payload in set boot source
    The vaild boot source parameter is:
    '{"Boot": {"BootSourceOverrideTarget": "Pxe",
    "BootSourceOverrideEnabled" : "Once"}}'
    
    not:
    '{"Boot": {"BootSourceOverrideTarget": "Pxe"},
    {"BootSourceOverrideEnabled" : "Once"}}'
    
    Change-Id: I319376f818b532a1511248a9f17ecd0a41540e88

--- redfish/types.py
+++ redfish/types.py
@@ -674,7 +674,7 @@ class Systems(Device):
         '''
         return self.set_parameter_json(
             '{"Boot": {"BootSourceOverrideTarget": "' +
-            target + '"},{"BootSourceOverrideEnabled" : "' + enabled + '"}}')
+            target + '", "BootSourceOverrideEnabled" : "' + enabled + '"}}')
 
 
 class SystemsCollection(BaseCollection):
