3538e43d13d315b7e2f29a13a7cb9b99d081698b
Debayan Ray <debayan.ray@gmail.com>
2017-02-22 02:47:00 -0500

    Aligning to Openstack unit tests running
    Prior to this patch python-redfish was using pytest for test
    running. Now, it has been moved to ostestr. For this, redfish
    package __init__.py imports have been modified. Will add the
    necessary changes to take care of backward compatibility.
    
    Change-Id: I1c5dd28a6cf6d2be3b1693c26210fb9039424cba
    Related-bug: #1663594

--- redfish/tests/__init__.py
+++ redfish/tests/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -11,6 +9,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-
-# from main import connect  # noqa: F401
--- redfish/tests/test_redfish.py
+++ redfish/tests/test_redfish.py
@@ -1,13 +1,31 @@
-# coding=utf-8
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Sample Test Class"""
 
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
+import unittest
 
 
-def test_fake():
-    print('This is a fake test')
-    assert (1 == 1)
+class SampleTestCase(unittest.TestCase):
+
+    def test_fake(self):
+        # ---------------------------------------------------------------------
+        # GIVEN
+        # ---------------------------------------------------------------------
+        print('This is a fake test')
+        # ---------------------------------------------------------------------
+        # WHEN
+        # ---------------------------------------------------------------------
+        value = 1
+        # -----------------------------------------------------------------------
+        # THEN
+        # -----------------------------------------------------------------------
+        self.assertEqual(1, value)
